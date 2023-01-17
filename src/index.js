04e78b09656e49bb79d86e3fb8459d4b8472ba38
Javier <javidx@gmail.com>
2023-01-17 18:05:19 +0200

    Added: Bootstrap HTML template structure Webpack config setup for bootstrap Class constructor to create to do list objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import "./style.scss";
+
+class Tasks {
+  constructor() {
+    this.tasks = []
+    this.items = 0
+  }
+
+  add = (task) => {
+    this.items += 1
+    task.index = this.items
+    this.tasks.push(task)
+  }
+
+  remove = () => {
+    this.items -= 1
+    this.tasks.pop()
+  }
+}
+
+class Task {
+  constructor(task) {
+    this.task = task
+    this.complete = false
+    this.index = 0
+  }
+}
+
+const newList = new Tasks
+
+newList.add(new Task('A task not completed 1'))
+newList.add(new Task('A task not completed 2'))
+newList.add(new Task('A task not completed 3'))
+
+console.log(newList);
--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,27 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
-      },
-    ],
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          }
+        }, {
+          loader: 'sass-loader'
+        }]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
