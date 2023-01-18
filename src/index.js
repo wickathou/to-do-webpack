f49b191f1c0e15bec86aeac8b3c26fd27c50bcf5
Javier <javidx@gmail.com>
2023-01-18 11:42:41 +0200

    Added: New private functions to generate DOM elements for the tasks new private function to add elements to localStorage New task input and event handler to add tasks

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import Task from './taskClass.js';
 import taskList from './tasklistClass.js'
 
 const taskListDom = document.getElementById('task-list');
+const newTaskDom = document.getElementById('new-task')
 
-taskList.add(new Task('One task with index 1'));
-taskList.add(new Task('Different task with index 2'));
-taskList.add(new Task('One last with index 3'));
+newTaskDom.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    taskList.add(new Task(e.target.value), taskListDom);
+    e.target.value = ''
+  };
+})
 
-taskList.tasks.forEach((task) => {
-  const li = document.createElement('li');
-  li.classList = 'p-4 list-group-item d-flex align-items-center justify-content-between';
-  li.innerHTML = `<div class="d-flex justify-content-between"><input class="form-check-input me-2" type="checkbox" value="" id="task-1"><label class="p-0 m-0 form-check-label" for="task-1">${task.description}</label><textarea class="p-0 m-0 border-0 hidden" name="" id="" cols="30" rows="1" placeholder="Test"></textarea></div><i class="fa-solid fa-ellipsis-vertical"></i>`;
-  taskListDom.appendChild(li);
-});
+// taskList.add(new Task('One task with index 1'), taskListDom);
+// taskList.add(new Task('Different task with index 2'), taskListDom);
+// taskList.add(new Task('One last with index 3'), taskListDom);
--- src/tasklistClass.js
+++ src/tasklistClass.js
@@ -4,10 +4,24 @@ class Tasks {
     this.items = 0;
   }
 
-  add = (task) => {
+  #addToLocalStorage = () => {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks))
+    console.log(localStorage.getItem('tasks'));
+  }
+
+  #newTaskElement = (task, taskListDom) => {
+    const li = document.createElement('li');
+    li.classList = 'p-4 list-group-item d-flex align-items-center justify-content-between';
+    li.innerHTML = `<div class="d-flex justify-content-between"><input class="form-check-input me-2" type="checkbox" value="" id="task-1"><label class="p-0 m-0 form-check-label" for="task-1">${task.description}</label><textarea class="p-0 m-0 border-0 hidden" name="" id="" cols="30" rows="1" placeholder="Test"></textarea></div><i class="fa-solid fa-ellipsis-vertical"></i>`;
+    taskListDom.appendChild(li);
+  }
+
+  add = (task, taskListDom) => {
     this.items += 1;
     task.index = this.items;
     this.tasks.push(task);
+    this.#addToLocalStorage()
+    this.#newTaskElement(task, taskListDom)
   }
 
   remove = () => {
