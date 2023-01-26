894aa262d4c2637bef68e9ef44280c4ac1fe3ef7
Andrea <andrearivas4cinfor@gmail.com>
2023-01-26 14:09:02 +0100

    Fix eslint

--- src/tests/clear.test.js
+++ src/tests/clear.test.js
@@ -13,13 +13,13 @@ describe('Clear all comleted', () => {
     const tasksLs = new Tasks();
     const task1 = new Task('task1');
     const task2 = new Task('task2');
-    const task3 = new Task ('task3');
+    const task3 = new Task('task3');
     // Add tasks and change status
     tasksLs.add(task1);
     tasksLs.status(true, 1);
-    tasksLs.add(task2)
+    tasksLs.add(task2);
     tasksLs.status(true, 2);
-    tasksLs.add(task3)
+    tasksLs.add(task3);
     tasksLs.status(true, 3);
     // act - call the function to delete all completed tasks
     tasksLs.deleteCompleted();
@@ -31,12 +31,12 @@ describe('Clear all comleted', () => {
     const tasksLs = new Tasks();
     const task1 = new Task('task1');
     const task2 = new Task('task2');
-    const task3 = new Task ('task3');
+    const task3 = new Task('task3');
     tasksLs.add(task1);
     tasksLs.status(true, 1);
-    tasksLs.add(task2)
+    tasksLs.add(task2);
     tasksLs.status(false, 2);
-    tasksLs.add(task3)
+    tasksLs.add(task3);
     tasksLs.status(true, 3);
     // act - call the function to delete all completed tasks
     tasksLs.deleteCompleted();
@@ -45,36 +45,36 @@ describe('Clear all comleted', () => {
   });
   test('Deleting all completed tasks from the DOM', () => {
   // Arange - Create tasks -
-  const tasksLs = new Tasks();
-  const task1 = new Task('task1');
-  const task2 = new Task('task2');
-  const task3 = new Task ('task3');
-  tasksLs.add(task1);
-  tasksLs.status(true, 1);
-  tasksLs.add(task2)
-  tasksLs.status(false, 2);
-  tasksLs.add(task3)
-  tasksLs.status(true, 3);
-  const container = document.createElement('div');
-  // act - call the function to delete all completed tasks
-  tasksLs.deleteCompleted();
-  tasksLs.generate(container);
-  //  asset - expect the length of tasks array to be 1
-  const taskElement = container.querySelectorAll('.task-list-element');
-  expect(taskElement.length).toBe(1);
+    const tasksLs = new Tasks();
+    const task1 = new Task('task1');
+    const task2 = new Task('task2');
+    const task3 = new Task('task3');
+    tasksLs.add(task1);
+    tasksLs.status(true, 1);
+    tasksLs.add(task2);
+    tasksLs.status(false, 2);
+    tasksLs.add(task3);
+    tasksLs.status(true, 3);
+    const container = document.createElement('div');
+    // act - call the function to delete all completed tasks
+    tasksLs.deleteCompleted();
+    tasksLs.generate(container);
+    //  asset - expect the length of tasks array to be 1
+    const taskElement = container.querySelectorAll('.task-list-element');
+    expect(taskElement.length).toBe(1);
   });
   test('Deleting all completed tasks from the DOM', () => {
     // Arange - Create tasks -
     const tasksLs = new Tasks();
     const task1 = new Task('task1');
     const task2 = new Task('task2');
-    const task3 = new Task ('task3');
-    const task4 = new Task ('task4');
+    const task3 = new Task('task3');
+    const task4 = new Task('task4');
     tasksLs.add(task1);
     tasksLs.status(true, 1);
-    tasksLs.add(task2)
+    tasksLs.add(task2);
     tasksLs.status(true, 2);
-    tasksLs.add(task3)
+    tasksLs.add(task3);
     tasksLs.status(false, 3);
     tasksLs.add(task4);
     tasksLs.status(false, 4);
@@ -85,5 +85,5 @@ describe('Clear all comleted', () => {
     //  asset - expect the length of tasks array to be 2
     const taskElement = container.querySelectorAll('.task-list-element');
     expect(taskElement.length).toBe(2);
-    });
+  });
 });
